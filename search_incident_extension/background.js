3fd5833e81157e00f09aa399d43e4b541d42e011
Szczepan Dwornicki <dwornickiszczepan@gmail.com>
2023-08-09 15:31:59 +0200

    Change message format. Add searching incident from clipboard functionality

--- search_incident_extension/background.js
+++ search_incident_extension/background.js
@@ -11,15 +11,15 @@ Background script of browser extension. Automates searching incident in ITSM.
 let intervalId;
 
 // Send message to the specified tab and log it.
-function sendMessage(tabID, message){
+function sendMessage(tabID, message) {
   chrome.tabs.sendMessage(tabID, message);
   console.log("Message was sent to the tab:", tabID)
 }
 
-// Open new tab of ITSM and start sending messages with incident ID to the newly created tab.
-function openItsmNewTabAndStartSendingMessages(incidentId){
-    // Open new tab of ITSM.
-    chrome.tabs.create({ url: "ITSM URL" }, function (tab) {
+// Open new tab of ITSM and start sending messages to the newly created tab.
+function openItsmNewTabAndStartSendingMessages(message) {
+  // Open new tab of ITSM.
+  chrome.tabs.create({ url: "ITSM URL" }, function (tab) {
 
     // Save ID of opened tab.
     const targetTabId = tab.id;
@@ -34,8 +34,8 @@ function openItsmNewTabAndStartSendingMessages(incidentId){
         // Unregister the event listener to avoid duplicate calls.
         chrome.tabs.onUpdated.removeListener(listener);
 
-        // Start sending incident ID to the tab every 0.5 second.
-        intervalId = setInterval(sendMessage, 500, targetTabId, {incId: incidentId});
+        // Start sending message to the tab every 0.5 second.
+        intervalId = setInterval(sendMessage, 500, targetTabId, message);
       }
     });
   });
@@ -43,36 +43,86 @@ function openItsmNewTabAndStartSendingMessages(incidentId){
 
 // Done only once at install time.
 chrome.runtime.onInstalled.addListener(() => {
-    // Create context menus.
-    chrome.contextMenus.create({
-        id: "1",
-        title: "Search the ITSM for \"%s\"", 
-        contexts: ["selection"], 
-    })
+  // Create context menus.
 
-    // Listener for messages from content script (tab).
-    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-      if (message.type === "contentScriptMessage") {
+  // Context menu to search incident from selected text.
+  chrome.contextMenus.create({
+    id: "selected_text",
+    title: "Search the ITSM for \"%s\"",
+    contexts: ["selection"],
+  })
+
+  // Context menu to search incident from clipboard.
+  chrome.contextMenus.create({
+    id: "clipboard",
+    title: "Search the ITSM for incident in the clipboard",
+    contexts: ["page"],
+  })
+
+  // Listener for messages from content script (tab) or popup script.
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+
+    if (message.destination === "background") {
+      if (message.source === "content") {
         const targetPage = message.targetPage;
         console.log("'targetPage' flag received from content script:", targetPage);
 
         // If tab returned targetPage = true, it means the target page (ITSM) was reached and searching the incident has begun.
         if (targetPage) {
-          // Stop sending incident ID to the tab.
+          // Stop sending messages to the tab.
           clearInterval(intervalId);
         }
       }
-      else if (message.type === "popupScriptMessage"){
+      else if (message.source === "popup") {
+
         const incidentId = message.incId;
-        console.log("Incident ID received from popup script: ", incidentId)
-        openItsmNewTabAndStartSendingMessages(incidentId);
+
+        // Prepare message.
+        const msg = {
+          source: "background",
+          destination: "content",
+          context: "search incident – popup",
+          incId: incidentId
+        };
+
+        console.log("Incident ID received from popup script: ", incidentId);
+
+        openItsmNewTabAndStartSendingMessages(msg);
       }
-    });
+    }
+  });
 });
 
-// Listener for the context menu.
-chrome.contextMenus.onClicked.addListener(function(info, tab){
+// Listener for the context menus.
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  // User selected incident ID and wants to search the ITSM for it.
+  if (info.menuItemId === "selected_text") {
     // Retrieve what user selected.
     const incidentId = info.selectionText;
-    openItsmNewTabAndStartSendingMessages(incidentId);
+
+    // Prepare message.
+    const msg = {
+      source: "background",
+      destination: "content",
+      context: "search incident – selected text",
+      incId: incidentId
+    };
+
+    openItsmNewTabAndStartSendingMessages(msg);
+  }
+  // User copied incident ID to the clipboard and wants to search the ITSM for it.
+  else if (info.menuItemId === "clipboard") {
+
+    console.log("clipboard use scenario");
+
+    // Prepare message.
+    const msg = {
+      source: "background",
+      destination: "content",
+      context: "search incident – clipboard"
+    };
+
+    // Access to the clipboard is in the content script, so the message will only tell that content script should retrieve the incident ID from the clipboard.
+    openItsmNewTabAndStartSendingMessages(msg);
+  }
 })
--- search_incident_extension/content.js
+++ search_incident_extension/content.js
@@ -14,104 +14,156 @@ let searchStarted = false;
 
 // Waits for HTML element to be visible.
 function waitForElementToBeVisible(selector) {
-    return new Promise((resolve) => {
-      const observer = new MutationObserver((mutationsList) => {
-        for (const mutation of mutationsList) {
-          if (mutation.type === 'childList' || mutation.type === 'attributes') {
-            const element = document.querySelector(selector);
-            if (element && element.offsetParent !== null) {
-              observer.disconnect();
-              resolve(element);
-            }
+  return new Promise((resolve) => {
+    const observer = new MutationObserver((mutationsList) => {
+      for (const mutation of mutationsList) {
+        if (mutation.type === 'childList' || mutation.type === 'attributes') {
+          const element = document.querySelector(selector);
+          if (element && element.offsetParent !== null) {
+            observer.disconnect();
+            resolve(element);
           }
         }
-      });
-  
-      observer.observe(document.documentElement, {
-        childList: true,
-        subtree: true,
-        attributes: true,
-        attributeFilter: ['style', 'hidden']
-      });
+      }
     });
+
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true,
+      attributes: true,
+      attributeFilter: ['style', 'hidden']
+    });
+  });
 }
 
 // Searches the ITSM for the incident. It replaces manual clicking.
-function searchForTheIncident(incidentId){
+function searchForTheIncident(incidentId) {
   // Click on "Search Incident" (wait untill it is visible). 
-  const searchIncidentElementSelector = 'search incident element selector';
+  const searchIncidentElementSelector = 'search incident element selector'
   const searchIncidentElement = document.querySelector(searchIncidentElementSelector);
 
   waitForElementToBeVisible(searchIncidentElementSelector).then((searchIncidentElement) => {
-      console.log('searchIncidentElement is now visible:', searchIncidentElement);
-      searchIncidentElement.click();
+    console.log('searchIncidentElement is now visible:', searchIncidentElement);
+    searchIncidentElement.click();
   });
 
   // Insert incident ID into the textarea element (wait untill it is visible). 
-  const textAreaElementSelector = 'text area element selector';
+  const textAreaElementSelector = 'text area element selector'
   const textAreaElement = document.querySelector(textAreaElementSelector);
 
   waitForElementToBeVisible(textAreaElementSelector).then((textAreaElement) => {
-      console.log('textAreaElement is now visible:', textAreaElement);
+    console.log('textAreaElement is now visible:', textAreaElement);
 
-      // Insert incident ID into the textarea.
-      textAreaElement.value = incidentId;
+    // Insert incident ID into the textarea.
+    textAreaElement.value = incidentId;
 
-      // Double clicking the textbox (incident ID) makes searching possible - it trickts system that incident ID was typed by the user.
-      // Without it, system does not recognize that ID was provided and refuses to continue.
-      const clickEvent  = document.createEvent ('MouseEvents');
-      clickEvent.initEvent ('dblclick', true, true);
-      textAreaElement.dispatchEvent (clickEvent);
+    // Double clicking the textbox (incident ID) makes searching possible - it trickts system that incident ID was typed by the user.
+    // Without it, system does not recognize that ID was provided and refuses to continue.
+    const clickEvent = document.createEvent('MouseEvents');
+    clickEvent.initEvent('dblclick', true, true);
+    textAreaElement.dispatchEvent(clickEvent);
 
-      // Click "Search" button (wait untill it is visible). 
-      const searchButtonSelector = 'search button selector';
-      const searchButton = document.querySelector(searchButtonSelector);
+    // Click "Search" button (wait untill it is visible). 
+    const searchButtonSelector = 'search button selector';
+    const searchButton = document.querySelector(searchButtonSelector);
 
-      waitForElementToBeVisible(searchButtonSelector).then((searchButton) => {
-          console.log('searchButton is now visible:', searchButton);
-          searchButton.click();
-      });
+    waitForElementToBeVisible(searchButtonSelector).then((searchButton) => {
+      console.log('searchButton is now visible:', searchButton);
+      searchButton.click();
+    });
   });
 }
 
-// Recives incident ID from background.js and checks whether it can start 
-// searching for that incident.
+// Recives message from the background script.
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  // If search already started, do not repeat it.
-  if(!searchStarted)
-  {
-    // Access the incident ID passed from the background script.
-    const incidentId = message.incId;
+  // Handle only those messages that are addressed to the content script. 
+  if (message.destination === "content") {
+    // If search already started, do not repeat it.
+    if (!searchStarted) {
+      // Check tab name.
+      if (document.title == "BMC Remedy (Search)") {
+        // Target page was reached.
+        // Send message to the background script, so it will stop sending the messages.
+        const msg = {
+          source: "content",
+          destination: "background",
+          context: "search incident",
+          targetPage: true
+        };
 
-    console.log("Received data in content script:", incidentId);
+        chrome.runtime.sendMessage(msg);
 
-    // Check tab name.
-    if(document.title == "BMC Remedy (Search)"){
-      // Target page was reached.
-      // Send message to the background script, so it will stop sending the messages with incident ID.
-      chrome.runtime.sendMessage({
-        type: "contentScriptMessage",
-        targetPage: true,
-      });
+        let incidentId;
 
-      // Search the ITSM for the incident.
-      searchStarted = true;
-      searchForTheIncident(incidentId);
-      console.log("Search started")
-      
+        // Depending on the message context, get the incident ID from the message or from the clipboard.
+        if (message.context === "search incident – clipboard") {
+
+          console.log("clipboard scenario in the content");
+
+          // To avoid multiple searches, flag has to be set already here, before asking user for permissions,
+          // not just before actual search start. Promise mechanism makes it tricky here. 
+          searchStarted = true;
+
+          // Wait for "Home" element to be visible. 
+          // In clipboard use case, it will be later needed to click on "Home" element to force the script to continue.
+          const homeElementSelector = 'home element selector'
+          const homeElement = document.querySelector(homeElementSelector);
+
+          waitForElementToBeVisible(homeElementSelector).then((homeElement) => {
+            console.log('homeElement is now visible:', homeElement);
+            navigator.permissions.query({ name: 'clipboard-read' }).then(result => {
+              // If permission to read the clipboard is granted or if the user will
+              // be prompted to allow it, then proceed.
+              if (result.state === 'granted' || result.state === 'prompt') {
+                navigator.clipboard.readText()
+                  .then(text => {
+                    incidentId = text;
+
+                    console.log("Incident ID in the content script:", incidentId);
+
+                    // Click on "Home" element. It is needed to force
+                    // the script to continue.  
+                    homeElement.click();
+
+                    // Search the ITSM for the incident.
+                    searchForTheIncident(incidentId);
+                    console.log("Search started")
+                  })
+                  .catch(err => {
+                    console.error('Failed to read clipboard contents: ', err);
+                  });
+              }
+            });
+          });
+        }
+        else {
+          console.log("selection or popup scenario in the content");
+          incidentId = message.incId;
+
+          console.log("Incident ID in the content script:", incidentId);
+
+          // Search the ITSM for the incident.
+          searchStarted = true;
+          searchForTheIncident(incidentId);
+          console.log("Search started")
+        }
+      }
+      else {
+        // Target page was not reached. Send message to the background script.
+        const msg = {
+          source: "content",
+          destination: "background",
+          context: "search incident",
+          targetPage: false,
+          docTitle: document.title
+        };
+        chrome.runtime.sendMessage(msg);
+      }
     }
-    else{
-      // Target page was not reached. Send message to the background script.
-      chrome.runtime.sendMessage({
-        type: "contentScriptMessage",
-        targetPage: false,
-        docTitle: document.title
-      });
+    else {
+      console.log("Search already started")
     }
   }
-  else{
-    console.log("Search already started")
-  }   
 });
 
 
